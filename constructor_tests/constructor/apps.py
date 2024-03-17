eeb056296519dd728067133287f9076c9a28c7ea
Sort7 <sort700007@gmail.com>
2024-03-17 20:05:48 +0300

    delit constructor2

--- constructor_tests/constructor/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConstructorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'constructor'
-    verbose_name = 'Конструктор тестов'  # Задаем отображение имени всего приложения, отображаемое в админке
-
